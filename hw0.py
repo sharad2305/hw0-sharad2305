15c7424e8935a1a5c912af5caa1f220d959933ab
sharad2305 <np796@cummins.com>
2019-10-22 17:07:18 +0530

    Create hw0.py

---
+++ hw0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib as mp
+print(np.__version__)
+print(sp.__version__)
+print(mp.__version__)
